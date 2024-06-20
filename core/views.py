41736b265b6dd28c6cd58e90918ff142478610dc
usr1 <test@world.com>
2024-06-20 06:09:27 +0000

    Initial commit

---
+++ core/views.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+
+from items.models import Category, Item
+from .forms import SignupForm
+# Create your views here.
+
+def index(request):
+    items = Item.objects.filter(is_sold=False)
+    categories = Category.objects.all()
+    print('Items:', items)
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'items': items
+    })
+
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+   })
---
+++ items/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import *
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from .forms import NewItemForm, EditItemForm
+
+
+def items(request):
+    query = request.GET.get('query')
+    # category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'items/items.html', {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        # 'category_id': int(category_id),
+    })
+
+
+def detail(request, pk):
+    items = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=items.category, is_sold=False).exclude(pk=pk)[0:3]
+
+    return render(request, 'items/detail.html', {
+        'items': items,
+        'related_items': related_items
+    })
+
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('items:detail', pk=item.id)
+    form = NewItemForm()
+
+    return render(request, 'items/form.html', {
+        'title': 'New Item',
+        'form': form
+    })
+
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            item.save()
+
+            return redirect('items:detail', pk=item.id)
+    form = EditItemForm(instance=item)
+
+    return render(request, 'items/form.html', {
+        'title': 'Edit Item',
+        'form': form
+    })
